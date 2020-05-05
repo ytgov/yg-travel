0312b854752b0501a5de460372355d13b7af9c07
Ryan Agar <ryanjagar@gmail.com>
2020-05-05 13:50:06 -0700

    new hosting for vue app using express

---
+++ client/server/app.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+var express = require('express');  
+var history = require('connect-history-api-fallback');
+
+require('dotenv').config()
+
+var port = process.env.PORT || 8080;
+var app = express();  
+
+app.use(history({
+    verbose: true}));
+
+app.use('',express.static('dist'))
+
+app.listen(port);  
+console.log(`Travel Portal App listening on internal port ${port}`)
