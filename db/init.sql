1d4a6d7f2cccd10e243342808c51fa3972ccb1f7
Max Parker <maxrparker@gmail.com>
2020-05-06 12:41:14 -0700

    sql init

---
+++ db/init.sql
@@ -0,0 +1,36 @@
+CREATE TABLE communities(
+    id SERIAL,
+    name varchar(32)
+);
+
+CREATE TABLE communityGroups(
+    id SERIAL,
+    name varchar(32)
+);
+
+CREATE TABLE travelNotices(
+    id SERIAL,
+    name varchar(64),
+  	travellers integer,
+  	arivalDate date,
+  	returnDate date,
+  	requireAssistance boolean,
+  	contactedCommunity boolean,
+  	phone varchar(16),
+  	email varchar(64),
+  	purpose varchar(256),
+  	code varchar(32),
+  	department varchar(32),
+    fnContact boolean,
+    muContact boolean,
+    otherContact boolean,
+    other varchar(32)
+);
+
+INSERT INTO communities (name) values
+('Whitehorse'),('Carmacks'),('Dawson City'),('Beaver Creek'),('Burwash Landing'),('Tagish'),('Carcross'),
+('Faro'),('Haines Junction'),('Mayo'),('Mount Lorne'),('Old Crow'),('Pelly Crossing'),('Ross River'),('Teslin'),('Watson Lake');
+
+
+INSERT INTO communityGroups (name) values
+('First Nations'),('Municipality'),('Other');
