9a286becf8d25804e6b73f89ffe93fef5482d9d4
Ryan Agar <ryanjagar@gmail.com>
2020-05-24 09:28:36 -0700

    shift to env

--- web/src/main.js
+++ web/src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 import axios from 'axios'
 
-//console.log(`API Base: ${process.env.VUE_APP_API_BASE}`)
+console.log(`API Base: ${process.env.VUE_APP_API_BASE}`)
 Vue.config.productionTip = false
 Vue.use({
     install (Vue) {
