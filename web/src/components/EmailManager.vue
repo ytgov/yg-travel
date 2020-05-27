92997495f494b7b641ce203c7aafd2f35cf5644a
Max Parker <maxrparker@gmail.com>
2020-05-27 15:52:55 -0700

    more email stuff. deleting email entries

--- web/src/components/EmailManager.vue
+++ web/src/components/EmailManager.vue
@@ -15,7 +15,8 @@
           <tr v-for="entry in entries" :key="entry.id">
             <td>{{ entry.email }}</td>
             <td>{{ entry.frequency }}</td>
-            <v-btn color="#6f9d2a" class="white--text">Remove
+            <v-btn icon color="red">
+              <v-icon>mdi-close</v-icon>
             </v-btn>
           </tr>
         </tbody>
@@ -23,15 +24,16 @@
     </v-simple-table>
     <v-row>
       <v-col>
-        <v-text-field label="Email" required filled md="2"></v-text-field>
+        <v-text-field label="Email" required filled md="2" v-model="subscribe.email"></v-text-field>
       </v-col>
       <v-col>
         <v-select label="Frequency"
                   :items="options"
+                  v-model="subscribe.frequency"
                   filled></v-select>
       </v-col>
       <v-col>
-        <v-btn color="#6f9d2a" class="white--text">Add
+        <v-btn color="#6f9d2a" class="white--text" @click="addEmail">Add
         </v-btn>
       </v-col>
     </v-row>
@@ -43,18 +45,26 @@
   import urls from '../urls'
   export default {
     name: 'EmailManager',
-    data: () => ({
-      options: ['Immediately', 'Weekly', 'Both'],
-      entries: []
-    }),
     props: {
       community: String
     },
+    data: () => ({
+      options: ['Immediately', 'Weekly', 'Both'],
+      entries: [],
+      subscribe: {
+        email: '',
+        frequency: ''
+      }
+    }),
     methods: {
       getEmails() {
         this.$api.get(urls.getEmailsByCommunity + this.community).then(response => {
           this.entries = response.data
         })
+      },
+      addEmail() {
+        this.subscribe.community = this.community
+        this.$api.post(urls.createEmail, this.subscribe).then(() => {this.getEmails()})
       }
     },
     filters: {},
--- web/src/urls.js
+++ web/src/urls.js
@@ -1,15 +1,15 @@
-const API_BASE = 'api/v1';
 export default {
-  communities: API_BASE + "/getCommunities",
-  communityGroups: API_BASE + "/getCommunityGroups",
-  departments: API_BASE + "/getDepartments",
-  createNotice: API_BASE + "/createNotice",
-  getNotices: API_BASE + "/getNotices",
-  updateNotice: API_BASE + "/updateNotice",
-  getNotice: API_BASE + "/getNotice/",
-  getReport: API_BASE + "/reports/",
-  getEmails: API_BASE + "/getEmails",
-  getEmailsByCommunity: API_BASE + "/getEmailsByCommunity/",
-  createEmail: API_BASE + "/createEmail",
-  updateEmail: API_BASE + "/updateEmail/"
+  communities: "/getCommunities",
+  communityGroups: "/getCommunityGroups",
+  departments: "/getDepartments",
+  createNotice: "/createNotice",
+  getNotices: "/getNotices",
+  updateNotice: "/updateNotice",
+  getNotice: "/getNotice/",
+  getReport: "/reports/",
+  getEmails: "/getEmails",
+  getEmailsByCommunity: "/getEmailsByCommunity/",
+  createEmail: "/createEmail",
+  updateEmail: "/updateEmail/",
+  deleteEmail: "/deleteEmail/"
 }
