5a99f687b89072790d7290d008d31dbbff00de82
Max Parker <maxrparker@gmail.com>
2020-06-01 11:55:39 -0700

    hold

--- web/src/components/EmailManager.vue
+++ web/src/components/EmailManager.vue
@@ -1,41 +1,45 @@
 <template>
   <v-container>
-    {{community}}
-    <v-simple-table>
-      <template v-slot:default>
-        <thead>
-          <tr>
-            <th class="text-left">Email</th>
-            <th class="text-left">Frequency</th>
-            <th class="text-left">Remove</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="entry in entries" :key="entry.id">
-            <td>{{ entry.email }}</td>
-            <td>{{ entry.frequency }}</td>
-            <v-btn icon color="red" v-on:click="deleteEmail(entry)">
-              <v-icon>mdi-close</v-icon>
-            </v-btn>
-          </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
-    <v-row>
-      <v-col>
-        <v-text-field label="Email" required filled md="2" v-model="subscribe.email"></v-text-field>
-      </v-col>
-      <v-col>
-        <v-select label="Frequency"
-                  :items="options"
-                  v-model="subscribe.frequency"
-                  filled></v-select>
-      </v-col>
-      <v-col>
-        <v-btn color="#6f9d2a" class="white--text" @click="addEmail">Add
-        </v-btn>
-      </v-col>
-    </v-row>
+    <v-card style="padding: 15px;">
+      Email Notifications
+      <v-simple-table>
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th class="text-left">Email</th>
+              <th class="text-left">Frequency</th>
+              <th class="text-left">Remove</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="entry in entries" :key="entry.id">
+              <td>{{ entry.email }}</td>
+              <td>{{ entry.frequency }}</td>
+              <v-btn icon color="red" v-on:click="deleteEmail(entry)">
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+      <v-row>
+        <v-col>
+          <v-text-field label="Email" required filled md="2" v-model="subscribe.email"></v-text-field>
+        </v-col>
+        <v-col>
+          <v-select label="Frequency"
+                    :items="options"
+                    v-model="subscribe.frequency"
+                    :rules="emailRules"
+                    filled></v-select>
+        </v-col>
+        <v-col>
+          <v-btn color="#ffa500" class="white--text" @click="addEmail">Add
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-card>
+
   </v-container>
 
 </template>
@@ -53,7 +57,9 @@
       subscribe: {
         email: '',
         frequency: ''
-      }
+      },
+      emailRules: [v => !!v || 'E-mail is required', v => /.+@.+\..+/.test(v) || 'E-mail must be valid'],
+
     }),
     methods: {
       getEmails() {
